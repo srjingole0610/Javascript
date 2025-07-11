c0eaa863c18d17bb2580ef6879a7d700b7615f4f
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2025-07-11 21:46:42 +0530

    docs: update README and add starter files for fundamentals
    - Add project structure section to README
    - Include basic HTML template and JavaScript starter files
    - Added javascript code about values and variables
    - Document usage instructions in README

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,39 @@
+//JS BASICS
+
+let js = "Am I learning JS?";
+if (js == "Am I learning JS?") {
+  console.log("Yes");
+  // alert('Js is amazing')
+}
+console.log(40 + 8 + 23 - 10);
+
+//JS Values and variables
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Suraj";  //Variable Declaration
+console.log(firstName);  //Variable Usage
+console.log(firstName);
+console.log(firstName);
+
+// // let 3year = 3;  //Variable name cannot start with numbers
+// // let $function = 27;  //Variable name cannot start with $ and _ (Dollar and underscore are not valid in JS)
+
+// let first&last = "Suraj Ingole";  //Variable name cannot contain & (Ampersand)
+// let new = 27;  //Variable name cannot be a reserved keyword (new is a reserved keyword)
+let _function = 27;  //Variable name can contain _ (underscore)
+console.log(_function);
+
+let $function = 35;  //Variable name can contain $ (dollar)
+console.log($function);
+
+let Person = 'jonas' //We should follow this naming convention
+let person = 'martha' //We should follow this naming convention or camelCase
+
+const PI = 3.1415 //Constant value 
+
+//Should use descriptive names for variables
+let myFirstJob = "Coder"
+let myCurrentJob = "Fullstack Developer"
+
--- README.md
+++ README.md
@@ -4,6 +4,14 @@
 
 This project is a JavaScript-based application that demonstrates modern JavaScript programming concepts and best practices.
 
+## Project Structure
+
+- 01-Fundamentals-Part-1/
+  - starter/
+    - index.html
+    - script.js
+- README.md
+
 ## Features
 
 - Modern ES6+ syntax
@@ -16,3 +24,7 @@ This project is a JavaScript-based application that demonstrates modern JavaScri
 - Node.js (v14.0.0 or higher)
 - npm (v6.0.0 or higher)
 - Modern web browser
+
+## Usage
+
+Open `01-Fundamentals-Part-1/starter/index.html` in a modern web browser to run the application.
