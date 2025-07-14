c214869aa79d548e98407b9de9e25b7a94c7c0b3
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2025-07-14 20:18:42 +0530

    docs(script): add function examples and explanations
    Add examples of function declarations, invocations, and parameter usage to demonstrate JavaScript function fundamentals. Includes examples of functions with parameters and return values.

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -14,3 +14,31 @@ if (hasDriversLicense) {
 //Strict Mode will throw an error if we try to declare a variable with a reserved keyword
 // const interface = "Audio";
 // const private = 534;
+
+// *********************************** Functions **************************************
+
+//Function Declaration
+function logger() {
+  console.log("I am logger function!");
+}
+
+//Invoke Function or Call Function or Run Function
+logger();
+logger();
+logger();
+
+//Function with Parameters and return value
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5,0); // Passing arguments to the function
+console.log(appleJuice);
+
+const orangeJuice = fruitProcessor(0,4);
+console.log(orangeJuice);
+
+const mixedJuice = fruitProcessor(2,4);
+console.log(mixedJuice);
