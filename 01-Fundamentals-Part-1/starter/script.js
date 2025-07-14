2556bd477feae34694882a18094b851f3dad2b79
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2025-07-14 18:47:05 +0530

    feat: add switch statement for day-based task planning
    Implement a switch statement to handle different tasks based on the day of the week. The statement covers all weekdays and weekends with appropriate default case handling.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -362,3 +362,30 @@ if(hasDriversLicense && hasGoodVision && !isTired){
 }else{
   console.log("Someone else should drive..............");
 }
+
+//************************************* Switch Statements **************************************
+
+const day = "Monday";
+
+switch (day){
+  case "Monday":
+    console.log("Plan course structure");
+    console.log("Go to coding meetup  ");
+    break;
+    case "Tuesday":
+    console.log("Prepare theory videos");
+    break;
+    case "Wednesday":
+    case "Thursday":
+    console.log("Write code examples");
+    break;
+    case "Friday":
+    console.log("Record videos");
+    break;
+    case "Saturday":
+    case "Sunday":
+    console.log("Enjoy the weekend");
+    break;
+    default:
+    console.log("Not a valid day");
+}
