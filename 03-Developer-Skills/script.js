b0ace63a9e02bc65fab40cee39a47e76bbfb7d52
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2025-07-17 02:09:20 +0530

    fix: correct temperature calculation bugs and add debugging
    - Fix measurement.value parsing by converting prompt input to Number
    - Add console logging for debugging temperature calculations
    - Fix min temperature initialization in calcTempAmplitudeBug

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -70,3 +70,47 @@ const calcTempAmplitudeNew = function (temp1, temp2) {
 
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 6, 5]);
 console.log(amplitudeNew);
+
+///////////////////////////////////////
+// Debugging with the Console and Breakpoints
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // C) FIXME:
+    // value: prompt('Degree Celsius:'),
+    value: Number(prompt('Degree Celsius:')),
+  };
+  // B) FIND THE BUG
+  console.log('measurement', measurement);
+  console.table(measurement);
+  console.log(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) Identify the bug
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  // B) FIND THE BUG
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
