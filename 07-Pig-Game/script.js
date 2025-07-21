a21771aea42024cd01e935bd6cfcc779df8e7125
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2025-07-21 20:54:18 +0530

    feat: implement dice rolling functionality and initialize game state

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -6,14 +6,42 @@ const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const name0El = document.getElementById('name--0');
 const name1El = document.getElementById('name--1');
-const diceEl = document.querySelector('.dice')
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.tn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const currentScore0El = document.getElementById('current--0');
+const currentScore1El  = document.getElementById('current--1');
 
+let currentScore = 0;
+
+//Starting conditions for the Game
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+// Generate Random Number
+const GenerateRandomDice = function () {
+  const diceValue = Math.trunc(Math.random() * 6) + 1;
+  return diceValue;
+};
 
+//Rolling Dice condition
+btnRoll.addEventListener('click', function () {
+  // Generate a random dice roll
+  const dice = GenerateRandomDice();
+  //Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `../images/dice-${dice}.png`;
 
+  //Check for rolled 1: if true, switch to next player;
+  if (dice !== 1) {
+    //Add dice to the current score
+    currentScore += dice;
+    currentScore0El.textContent = currentScore;
+  } else {
+  }
+});
 
 // Theme Management
 function toggleTheme() {
